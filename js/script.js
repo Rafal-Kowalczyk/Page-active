7229def46cea4da152080ed30bb17a24e218a602
Rafał Kowalczyk <rako.r.kowalczyk@interia.pl>
2018-12-06 22:13:00 +0100

    Add js folder and script

---
+++ js/script.js
@@ -0,0 +1,10 @@
+$(function() {
+    // scrollowanie strony
+    $('a').on('click', function(e) {
+        e.preventDefault();
+        $('html, body').animate({
+            scrollTop: $( $(this).attr('href')).offset().top
+        }, 2500);
+    });
+
+});
