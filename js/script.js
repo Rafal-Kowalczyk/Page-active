b006ee10d087e6c73041d0884fc86e593ca0c159
Rafał Kowalczyk <rako.r.kowalczyk@interia.pl>
2018-12-06 22:17:05 +0100

    Add beautify file

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
-$(function() {
+$(function () {
     // scrollowanie strony
-    $('a').on('click', function(e) {
+    $('a').on('click', function (e) {
         e.preventDefault();
         $('html, body').animate({
-            scrollTop: $( $(this).attr('href')).offset().top
+            scrollTop: $($(this).attr('href')).offset().top
         }, 2500);
     });
 
